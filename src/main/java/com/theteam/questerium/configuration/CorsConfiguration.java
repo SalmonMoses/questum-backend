abef5b69bdee27a5741c2d912b4e19d23520650d
Mikhail Melamed <mikhail.melamed@gmail.com>
2020-05-06 16:21:01 +0300

    Починил CORS(как же я соскучился...)

--- src/main/java/com/theteam/questerium/configuration/CorsConfiguration.java
+++ src/main/java/com/theteam/questerium/configuration/CorsConfiguration.java
@@ -1,8 +1,10 @@
 package com.theteam.questerium.configuration;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
